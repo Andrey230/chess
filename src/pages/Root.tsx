7e0ccbd16a1783a119c8a34645aabe6b336c439e
a.bilous <a.bilous@softum.io>
2024-10-29 12:13:15 +0100

    init

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -32,6 +32,8 @@ export default function Root(){
     const [chessboardBg, setChessboardBg] = useState(defaultBg);
     const [boardOrientation, setBoardOrientation] = useState('white');
     const [moveCounter, setMoveCounter] = useState(0);
+    const [pgn, setPgn] = useState('');
+    const [showPgnInput, setShowPgnInput] = useState(false);
 
     useEffect(() => {
         if(isRandom){
@@ -39,7 +41,7 @@ export default function Root(){
             pickRandomVariant();
         }else{
             setRandomVariant(null);
-            setGame(new Chess);
+            setGame(new Chess());
         }
 
         setMoves([]);
@@ -54,53 +56,6 @@ export default function Root(){
             }, 1000);
         }
     }, [chessboardBg]);
-
-    useEffect(() => {
-        const handleKeydown = (event) => {
-            if(isRandom){
-                return;
-            }
-
-            if(moves.length === 0){
-                return;
-            }
-
-            let fen = null;
-
-            if (event.key === 'ArrowLeft') {
-
-                const returnMove = moves[moveCounter - 1];
-                if(!returnMove){
-                    return;
-                }
-                fen = returnMove.before;
-
-                setMoveCounter(moveCounter - 1);
-            } else if (event.key === 'ArrowRight') {
-                const returnMove = moves[moveCounter];
-                if(!returnMove){
-                    return;
-                }
-                fen = returnMove.after;
-                setMoveCounter(moveCounter + 1);
-            }else{
-                return;
-            }
-
-            if(!fen){
-                return;
-            }
-
-            const newGame = new Chess(fen);
-            setGame(newGame);
-        };
-
-        window.addEventListener('keydown', handleKeydown);
-
-        return () => {
-            window.removeEventListener('keydown', handleKeydown);
-        };
-    }, [moveCounter]);
 
     function makeAMove(move) {
         const gameCopy = getGameCopy();
@@ -291,10 +246,73 @@ export default function Root(){
         setBoardOrientation(boardOrientation === 'white' ? 'black' : 'white');
     }
 
+
+    const loadPgnVariant = () => {
+        try {
+            const newGame = new Chess();
+            newGame.loadPgn(pgn);
+            const historyMoves = newGame.history({
+                verbose: true,
+            });
+            setGame(newGame);
+            const newMoves = historyMoves.map((move) => {
+                return {
+                    from: move.from,
+                    to: move.to,
+                    san: move.san,
+                    before: move.before,
+                    after: move.after,
+                    color: move.color,
+                };
+            });
+            setMoves(newMoves);
+            setMoveCounter(newMoves.length);
+            setShowPgnInput(false);
+            setPgn('');
+        }catch (e){
+            console.log(e.message);
+        }
+    }
+
+    const changePgnInput = (e) => {
+        setPgn(e.target.value);
+    }
+
     const logout = () => {
         setToken(null);
         localStorage.removeItem('jwt');
         navigate('/login');
+    }
+
+    const stepBack = () => {
+        if(isRandom){
+            return;
+        }
+
+        const returnMove = moves[moveCounter - 1];
+        if(!returnMove){
+            return;
+        }
+
+        const newGame = new Chess(returnMove.before);
+        setMoveCounter(moveCounter - 1);
+        setGame(newGame);
+    }
+
+    const stepForward = () => {
+        if(isRandom){
+            return;
+        }
+        if(moveCounter >= moves.length){
+            return;
+        }
+        const nextMove = moves[moveCounter];
+        if(!nextMove){
+            return;
+        }
+        const newGame = new Chess(nextMove.after);
+        setMoveCounter(moveCounter + 1);
+        setGame(newGame);
     }
 
 
@@ -307,7 +325,7 @@ export default function Root(){
             <div className="min-h-lvh bg-base-300 pb-20">
                 {randomVariant ? <RandomVariant variant={randomVariant} /> : ''}
                 <div className="flex justify-center pt-28 gap-5 items-start">
-                    <div className="p-5 shadow-2xl bg-base-100 rounded-lg w-80">
+                    <div className="p-5 shadow-xl bg-base-100 rounded-lg w-80">
 
                             <Outlet context={{setShowChessboardButtons, setVariants, variants, setRepositoryId, setToken, setRandom}}/>
                         {/*<h3 className="text-xl font-bold mb-5">Variations</h3>*/}
@@ -324,15 +342,26 @@ export default function Root(){
                         {/*    })}*/}
                         {/*</div>*/}
                     </div>
-                    <div id="chess_board" className={`p-5 shadow-2xl bg-base-100 rounded-lg ${chessboardBg}`}>
-                        <Chessboard
-                            position={game.fen()}
-                            onPieceDrop={onDrop}
-                            boardOrientation={boardOrientation}
-                            //boardOrientation={"black"}
-                        />
+                    <div>
+                        <div id="chess_board" className={`p-5 shadow-xl bg-base-100 rounded-lg ${chessboardBg}`}>
+                            <Chessboard
+                                position={game.fen()}
+                                onPieceDrop={onDrop}
+                                boardOrientation={boardOrientation}
+                                //boardOrientation={"black"}
+                            />
+                        </div>
+
+                        <div className="flex mt-3 justify-center gap-5">
+                            <button className={"btn shadow-xl " + (moveCounter > 0 ? "btn-success" : "btn-disabled")} onClick={stepBack}>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 fill-base-100" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
+                            </button>
+                            <button className={"btn shadow-xl " + (moveCounter < moves.length ? "btn-success" : "btn-disabled")} onClick={stepForward}>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 fill-base-100" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
+                            </button>
+                        </div>
                     </div>
-                    <div className="p-5 shadow-2xl bg-base-100 rounded-lg w-80">
+                    <div className="p-5 shadow-xl bg-base-100 rounded-lg w-96">
 
                         {showChessboardButtons ? <>
                         <div className="flex gap-2">
@@ -345,6 +374,9 @@ export default function Root(){
                             <button className="btn btn-primary" onClick={changeBoardOrientation}>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 fill-base-100" viewBox="0 0 512 512"><path d="M105.1 202.6c7.7-21.8 20.2-42.3 37.8-59.8c62.5-62.5 163.8-62.5 226.3 0L386.3 160 352 160c-17.7 0-32 14.3-32 32s14.3 32 32 32l111.5 0c0 0 0 0 0 0l.4 0c17.7 0 32-14.3 32-32l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 35.2L414.4 97.6c-87.5-87.5-229.3-87.5-316.8 0C73.2 122 55.6 150.7 44.8 181.4c-5.9 16.7 2.9 34.9 19.5 40.8s34.9-2.9 40.8-19.5zM39 289.3c-5 1.5-9.8 4.2-13.7 8.2c-4 4-6.7 8.8-8.1 14c-.3 1.2-.6 2.5-.8 3.8c-.3 1.7-.4 3.4-.4 5.1L16 432c0 17.7 14.3 32 32 32s32-14.3 32-32l0-35.1 17.6 17.5c0 0 0 0 0 0c87.5 87.4 229.3 87.4 316.7 0c24.4-24.4 42.1-53.1 52.9-83.8c5.9-16.7-2.9-34.9-19.5-40.8s-34.9 2.9-40.8 19.5c-7.7 21.8-20.2 42.3-37.8 59.8c-62.5 62.5-163.8 62.5-226.3 0l-.1-.1L125.6 352l34.4 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L48.4 288c-1.6 0-3.2 .1-4.8 .3s-3.1 .5-4.6 1z"/></svg>
                             </button>
+                            <button className="btn btn-warning" onClick={() => {setShowPgnInput(true)}}>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 fill-base-100" viewBox="0 0 512 512"><path d="M168 80c-13.3 0-24 10.7-24 24l0 304c0 8.4-1.4 16.5-4.1 24L440 432c13.3 0 24-10.7 24-24l0-304c0-13.3-10.7-24-24-24L168 80zM72 480c-39.8 0-72-32.2-72-72L0 112C0 98.7 10.7 88 24 88s24 10.7 24 24l0 296c0 13.3 10.7 24 24 24s24-10.7 24-24l0-304c0-39.8 32.2-72 72-72l272 0c39.8 0 72 32.2 72 72l0 304c0 39.8-32.2 72-72 72L72 480zM176 136c0-13.3 10.7-24 24-24l96 0c13.3 0 24 10.7 24 24l0 80c0 13.3-10.7 24-24 24l-96 0c-13.3 0-24-10.7-24-24l0-80zm200-24l32 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-32 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80l32 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-32 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zM200 272l208 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-208 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80l208 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-208 0c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg>
+                            </button>
                             <button className={isRandom ? 'btn btn-success' : 'btn btn-active'} onClick={switchRandom}>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 fill-base-100" viewBox="0 0 448 512"><path d="M96 48L82.7 61.3C70.7 73.3 64 89.5 64 106.5l0 132.4c0 10.7 5.3 20.7 14.2 26.6l10.6 7c14.3 9.6 32.7 10.7 48.1 3l3.2-1.6c2.6-1.3 5-2.8 7.3-4.5l49.4-37c6.6-5 15.7-5 22.3 0c10.2 7.7 9.9 23.1-.7 30.3L90.4 350C73.9 361.3 64 380 64 400l320 0 28.9-159c2.1-11.3 3.1-22.8 3.1-34.3l0-14.7C416 86 330 0 224 0L83.8 0C72.9 0 64 8.9 64 19.8c0 7.5 4.2 14.3 10.9 17.7L96 48zm24 68a20 20 0 1 1 40 0 20 20 0 1 1 -40 0zM22.6 473.4c-4.2 4.2-6.6 10-6.6 16C16 501.9 26.1 512 38.6 512l370.7 0c12.5 0 22.6-10.1 22.6-22.6c0-6-2.4-11.8-6.6-16L384 432 64 432 22.6 473.4z"/></svg>
                             </button>
@@ -356,6 +388,13 @@ export default function Root(){
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 fill-base-100" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
                                 </button>
                             </div> : ''}
+
+                                {showPgnInput ? <div className="flex gap-2 mt-3">
+                                    <input type="text" placeholder="Paste pgn" value={pgn} className="input input-bordered w-full max-w-xs" onChange={changePgnInput} />
+                                    <button className="btn btn-success" onClick={loadPgnVariant}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 fill-base-100" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+                                    </button>
+                                </div> : ''}
 
                             <details className="collapse collapse-arrow border-base-300 bg-base-200 border mt-5">
                                 <summary className="collapse-title text-xl font-medium">How it works ?</summary>
@@ -385,6 +424,14 @@ export default function Root(){
                                         </button>
 
                                         <p>Change chessboard orientation</p>
+                                    </div>
+
+                                    <div className="flex justify-start items-center gap-2 mt-3">
+                                        <button className="btn btn-warning">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 fill-base-100" viewBox="0 0 512 512"><path d="M168 80c-13.3 0-24 10.7-24 24l0 304c0 8.4-1.4 16.5-4.1 24L440 432c13.3 0 24-10.7 24-24l0-304c0-13.3-10.7-24-24-24L168 80zM72 480c-39.8 0-72-32.2-72-72L0 112C0 98.7 10.7 88 24 88s24 10.7 24 24l0 296c0 13.3 10.7 24 24 24s24-10.7 24-24l0-304c0-39.8 32.2-72 72-72l272 0c39.8 0 72 32.2 72 72l0 304c0 39.8-32.2 72-72 72L72 480zM176 136c0-13.3 10.7-24 24-24l96 0c13.3 0 24 10.7 24 24l0 80c0 13.3-10.7 24-24 24l-96 0c-13.3 0-24-10.7-24-24l0-80zm200-24l32 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-32 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80l32 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-32 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zM200 272l208 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-208 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80l208 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-208 0c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg>
+                                        </button>
+
+                                        <p>Load variant from PGN</p>
                                     </div>
 
                                     <div className="flex justify-start items-center gap-2 mt-3">
